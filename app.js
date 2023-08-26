3aa5ac5654cd5e27b483f5be6c1769c1087eefe8
VICTOR <victorzarbesu@gmail.com>
2023-08-26 17:07:17 -0300

    conectando

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("aplicacação está se conectando com o banco de dados")
 
-console.log ("o Victor acrescentou mais uma linha")
+console.log ("o Victor acrescentou mais uma linha de codigo")
 
